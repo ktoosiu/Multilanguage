8186a3c82b0b4e14bae3e1ec66686bb63f24aeb0
Bartek <ktoosiu@gmail.com>
2020-11-23 22:56:34 +0100

    handler proof of concept

--- Multilanguage/Program.cs
+++ Multilanguage/Program.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Multilanguage
 {
+    //https://refactoring.guru/pl/design-patterns/chain-of-responsibility/csharp/example
     public class Program
     {
         public static void Main(string[] args)
@@ -22,5 +23,87 @@ namespace Multilanguage
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+    }
+
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+
+        object Handle(object request);
+    }
+    //language podany
+    //eng language
+    //pl language
+    //any languge
+    abstract class AbstractHandler : IHandler
+    {
+        private IHandler _nextHandler;
+
+        public IHandler SetNext(IHandler handler)
+        {
+            this._nextHandler = handler;
+
+            // Returning a handler from here will let us link handlers in a
+            // convenient way like this:
+            // monkey.SetNext(squirrel).SetNext(dog);
+            return handler;
+        }
+
+        public virtual object Handle(object request)
+        {
+            if (this._nextHandler != null)
+            {
+                return this._nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+    class EnglishHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if ((request as string) == "english")
+            {
+                return "Język polski";
+
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class PolishHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "Polish")
+            {
+                return "Język polski";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
+    }
+
+    class DogHandler : AbstractHandler
+    {
+        public override object Handle(object request)
+        {
+            if (request.ToString() == "MeatBall")
+            {
+                return $"Dog: I'll eat the {request.ToString()}.\n";
+            }
+            else
+            {
+                return base.Handle(request);
+            }
+        }
     }
 }
