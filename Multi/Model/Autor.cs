3ab194099ae74b078501c928aeb824db973c191d
VinciDaKinG <pajakkamil@protonmail.com>
2020-11-26 20:11:55 +0100

    in Progres

---
+++ Multi/Model/Autor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multi.Model
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; }
+        
+
+    }
+}
---
+++ Multi/Model/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Multi.Model
+{
+    public class Book
+    {
+        
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string LanguageCode { get; set; }
+
+
+
+    }
+}
